cd78a5f4554b98f71a835a616f2f0a94f3f02deb
Maral Erfanian <me.maralerfanian@gmail.com>
2023-02-26 10:37:50 +0100

    added button block component

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="block">
+        Click Me
+    </div>
+</template>
+<script>
+export default {
+    props: ["delay"]
+}
+</script>
+<style>
+.block{
+    width: 400px;
+    border-radius: 20px;
+    background: #9beed5;
+    color: #444;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+    
+</style>
